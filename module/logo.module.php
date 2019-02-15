07d1c2501db84142d7231c94d7f0e91389fefa98
papiando (HOME) <info@papiando.com>
2019-02-15 19:47:58 -0400

    Create logo.module.php

---
+++ module/logo.module.php
@@ -0,0 +1,20 @@
+<?php
+namespace Cubo;
+
+defined('__CUBO__') || new \Exception("No use starting a class without an include");
+
+class LogoModule extends Addon {
+	// Render login form
+	public static function render($_Params) {
+		$html = '<a class="navbar-brand" href="/<cubo:param name=\'route\' />">';
+		if(!empty(Configuration::getParameter('brand-logo'))) {
+			$html .= '<img class="brand-logo" src="'.Configuration::getParameter('brand-logo').'" />';
+		}
+		if(!empty(Configuration::getParameter('brand-name'))) {
+			$html .= '<span class="brand-name">'.Configuration::getParameter('brand-name').'</span>';
+		}
+		$html .= '</a>';
+		return $html;				
+	}
+}
+?>
