f37b645d6cb2ff6d62166a47e6f0cd3915c2cb68
papiando (HOME) <info@papiando.com>
2019-02-14 20:31:47 -0400

    Contact controller and view

---
+++ controller/contact.controller.php
@@ -0,0 +1,9 @@
+<?php
+namespace Cubo;
+
+defined('__CUBO__') || new \Exception("No use starting a class without an include");
+
+class ContactController extends Controller {
+	protected $columns = "*";
+}
+?>
