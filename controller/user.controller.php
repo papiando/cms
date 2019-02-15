abbc9964a5190f1da5919fdb600b8fb857c65a9e
papiando (HOME) <info@papiando.com>
2019-02-14 23:45:19 -0400

    Login method now renders login article

--- controller/user.controller.php
+++ controller/user.controller.php
@@ -13,23 +13,29 @@ class UserController extends Controller {
 			http_response_code(Session::get('http_response'));
 			Session::delete('http_response');
 		}
-		$model = __CUBO__.'\\'.Application::getRouter()->getController();
-		$view = __CUBO__.'\\'.Application::getRouter()->getController().'view';
-		$method = Application::getRouter()->getMethod();
-		if(class_exists($view)) {
-			if(method_exists($view,$method)) {
-				// Call method and return output
-				self::$_View = new $view;
-				return self::$_View->$method();
+		// Call new router to login article
+		$_Router = new Router('/article/login');
+		$controller = __CUBO__.'\\'.$_Router->getController().'controller';
+		$method = $_Router->getMethod();
+		try {
+			if(class_exists($controller)) {
+				if(method_exists($controller,$method)) {
+					// Call the method and show the output
+					$_Controller = new $controller($_Router);
+					$output = $_Controller->$method();
+					return $output;
+				} else {
+					// Method does not exist for this controller
+					$controller = $_Router->getController();
+					throw new Error(['class'=>__CLASS__,'method'=>__METHOD__,'line'=>__LINE__,'file'=>__FILE__,'severity'=>1,'response'=>405,'message'=>"Controller '{$controller}' does not have the method '{$method}' defined"]);
+				}
 			} else {
-				// Method does not exist for this view
-				$view = Application::getRouter()->getController();
-				throw new Error(['class'=>__CLASS__,'method'=>__METHOD__,'line'=>__LINE__,'file'=>__FILE__,'severity'=>1,'response'=>405,'message'=>"View '{$view}' does not have the method '{$method}' defined"]);
+				// Controller not found
+				$controller = $_Router->getController();
+				throw new Error(['class'=>__CLASS__,'method'=>__METHOD__,'line'=>__LINE__,'file'=>__FILE__,'severity'=>1,'response'=>405,'message'=>"Controller '{$controller}' does not exist"]);
 			}
-		} else {
-			// View not found
-			$view = Application::getRouter()->getController();
-			throw new Error(['class'=>__CLASS__,'method'=>__METHOD__,'line'=>__LINE__,'file'=>__FILE__,'severity'=>1,'response'=>405,'message'=>"View '{$view}' does not exist"]);
+		} catch(Error $_Error) {
+			$_Error->showMessage();
 		}
 		return false;
 	}
