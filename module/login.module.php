d69e365dae4508ac3608d7ea8cc50365daacc116
papiando (HOME) <info@papiando.com>
2019-02-15 00:05:30 -0400

    Rendering of login module

--- module/login.module.php
+++ module/login.module.php
@@ -5,7 +5,7 @@ defined('__CUBO__') || new \Exception("No use starting a class without an includ
 
 class LoginModule extends Addon {
 	// Render login form
-	public static function render($html) {
+	public static function render() {
 		$html = '<form class="form-login" action="" method="post">';
 		$html .= '<div class="form-group"><label for="login">Login name</label><input type="text" name="login" id="login" value="" class="form-control" placeholder="Login name" required autofocus /></div>';
 		$html .= '<div class="form-group"><label for="password">Password</label><input type="password" name="password" id="password" value="" class="form-control" placeholder="Password" required /></div>';
--- plugin/module.plugin.php
+++ plugin/module.plugin.php
@@ -6,16 +6,21 @@ defined('__CUBO__') || new \Exception("No use starting a class without an includ
 class ModulePlugin extends Addon {
 	// Load module and render output
 	public static function loadModule($module) {
-		$_Module = Module::get($module);
-		if($_Module) {
-			return $_Module::render();
-		} else
-			return false;
+		$module = __CUBO__.'\\'.$module.'module';
+		if(class_exists($module)) {
+			$_Module = new $module;
+			if($_Module) {
+				return $_Module::render();
+			} else
+				return false;
+		} else {
+			// For the moment, skip any errors if the module does not exist
+		}
+		return false;
 	}
 	
 	// Replaces name tags in content by parameter values
 	public static function render($html) {
-		die("Starting Module Plugin");
 		if($html) {
 			return preg_replace_callback("/<cubo:module\s+name\s*=\s*[\'\"]([^\'\"]+)[\'\"]\s*\/>/i",function($matches) { return self::loadModule($matches[1]); },$html);
 		} else {
