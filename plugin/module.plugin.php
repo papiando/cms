c71db3224d2652e06073c34acff075e997d20619
papiando (HOME) <info@papiando.com>
2019-02-15 17:54:07 -0400

    Adding possibility to pass parameters in module tag

--- plugin/module.plugin.php
+++ plugin/module.plugin.php
@@ -5,12 +5,14 @@ defined('__CUBO__') || new \Exception("No use starting a class without an includ
 
 class ModulePlugin extends Addon {
 	// Load module and render output
-	public static function loadModule($module) {
+	public static function loadModule($module,$params) {
+		// Read content and parse as if query string; store in _Params
+		parse_str(str_replace(',','&',trim($params)),$_Params);
 		$module = __CUBO__.'\\'.$module.'module';
 		if(class_exists($module)) {
 			$_Module = new $module;
 			if($_Module) {
-				return $_Module::render();
+				return $_Module::render((object)$_Params);
 			} else
 				return false;
 		} else {
@@ -20,9 +22,10 @@ class ModulePlugin extends Addon {
 	}
 	
 	// Replaces name tags in content by parameter values
+	//   Tags should match: <cubo:module name="module-name" content="param1=first,param2=second" />
 	public static function render($html) {
 		if($html) {
-			return preg_replace_callback("/<cubo:module\s+name\s*=\s*[\'\"]([^\'\"]+)[\'\"]\s*\/>/i",function($matches) { return self::loadModule($matches[1]); },$html);
+			return preg_replace_callback("/<cubo:module\s+name\s*=\s*[\'\"]([^\'\"]+)[\'\"]\s+content\s*=\s*[\'\"]([^\'\"]*)[\'\"]\s*\/>/i",function($matches) { return self::loadModule($matches[1],$matches[2]); },$html);
 		} else {
 			return $html;
 		}
