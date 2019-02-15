78b24dfc20a24755684dcad3857a73ffcb9906df
papiando (HOME) <info@papiando.com>
2019-02-14 23:43:23 -0400

    Make router class final

--- framework/router.class.php
+++ framework/router.class.php
@@ -3,7 +3,7 @@ namespace Cubo;
 
 defined('__CUBO__') || new \Exception("No use starting a class without an include");
 
-class Router {
+final class Router {
 	private $controller;
 	private $format;
 	private $method;
