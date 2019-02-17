e882f2eecf63b75352de6e868c797460d9a4d07b
papiando (HOME) <info@papiando.com>
2019-02-17 00:55:40 -0400

    Image view and controller

---
+++ controller/image.controller.php
@@ -0,0 +1,9 @@
+<?php
+namespace Cubo;
+
+defined('__CUBO__') || new \Exception("No use starting a class without an include");
+
+class ImageController extends Controller {
+	protected $columns = "*";
+}
+?>
