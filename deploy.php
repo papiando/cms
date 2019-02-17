dcae24879706fc0b412d28ac615af76cad27fd9a
papiando (HOME) <info@papiando.com>
2019-02-17 15:21:38 -0400

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,4 +1,6 @@
 <?php
         exec("git pull >&1",$output,$return_var);
+		exec("find . -type f -exec chmod 644 {} \; >&1",$outnull,$return_var);
+		exec("find . -type d -exec chmod 755 {} \; >&1",$outnull,$return_var);
         print_r($output);
 ?>
