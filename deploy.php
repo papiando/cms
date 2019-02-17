af6e52f085853b6c4559f5ecf94944989f8c7618
papiando (HOME) <info@papiando.com>
2019-02-17 15:29:20 -0400

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,4 +1,4 @@
-<?php
+<?php	// Automatically pull updates (https://gist.github.com/nichtich/5290675)
         exec("git pull >&1",$output,$return_var);
 		exec("find . -type f -exec chmod 644 {} \; >&1",$outnull,$return_var);
 		exec("find . -type d -exec chmod 755 {} \; >&1",$outnull,$return_var);
