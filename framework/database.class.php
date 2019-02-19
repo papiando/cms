67d7be1179bc95ca5bd1aa5d43282d45046b4205
papiando (CBCS) <info@papiando.com>
2019-02-19 17:10:51 -0400

    Update database.class.php

--- framework/database.class.php
+++ framework/database.class.php
@@ -315,7 +315,7 @@ class Database {
 		$string = strtolower(trim($string));
 		$string = str_replace(array_keys($special_cases),array_values($special_cases),$string);
 		$string = preg_replace($accents_regex,'$1',htmlentities($string,ENT_QUOTES,'UTF-8'));
-		$string = preg_replace("/[^a-z0-9]/u","$separator",$string);
+		$string = preg_replace("/[^a-z0-9\._]/u","$separator",$string);
 		$string = preg_replace("/[$separator]+/u","$separator",$string);
 		return $string;
 	}
