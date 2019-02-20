02dcf89c5499de92482d81c228bd79e2da715b2a
papiando (HOME) <info@papiando.com>
2019-02-19 23:35:53 -0400

    Update form.class.php

--- framework/form.class.php
+++ framework/form.class.php
@@ -1,28 +1,20 @@
 <?php
-/**
- * @application    Cubo CMS
- * @type           Framework
- * @class          Form
- * @description    The form framework facilitates the correct display of forms
- * @version        1.2.0
- * @date           2018-02-05
- * @author         Dan Barto
- * @copyright      Copyright (C) 2017 - 2019 Papiando Riba Internet. All rights reserved.
- * @license        MIT License; see LICENSE.md
- */
 namespace Cubo;
 
 defined('__CUBO__') || new \Exception("No use starting a class without an include");
 
 class Form {
+	// Return query
 	public static function query($class,$filter = "1",$order = "`title`") {
 		return "SELECT `#`,`title` FROM `{$class}` WHERE {$filter} ORDER BY {$order}";
 	}
-	public static function selectFilter($params) {
+	
+	// Return selection
+	public static function select(&$params) {
 		!is_array($params) || $params = (object)$params;
 		$html = '<div class="form-group">';
 		$html .= '<label for="'.$params->name.'">'.$params->title.'</label>';
-		$html .= '<select id="'.$params->name.'" name="'.($params->prefix ?? '').$params->name.'" class="form-control'.($params->class ?? '').'"'.(isset($params->readonly) && $params->readonly ? ' readonly tabindex="-1"' : '').'>';
+		$html .= '<select id="'.$params->name.'" name="'.($params->prefix ?? '').str_replace('-','_',$params->name).'" class="'.$params->class.'"'.(isset($params->readonly) && $params->readonly ? ' readonly' : '').' tabindex="-1">';
 		$items = [];
 		if(isset($params->query)) {
 			$_Model = new Model;
@@ -38,10 +30,26 @@ class Form {
 		$html .= '</div>';
 		return $html;
 	}
-	// Filter for text search
-	public static function textFilter($params) {
+	
+	// Return filter selection
+	public static function selectFilter(&$params) {
+		return self::select($params);
+	}
+	
+	// Return textarea
+	public static function textarea(&$params) {
 		!is_array($params) || $params = (object)$params;
-		return '<div class="form-group"><label for="'.$params->id.'">'.$params->label.'</label><input id="'.$params->prefix.$params->id.'" name="-'.$params->id.'" class="form-control" type="text" placeholder="'.$params->label.'" value="'.$params->value.'" /></div>';
+		$html = '<div class="form-group'.(isset($params->width) ? ' grid-column-'.$params->width : '').'">';
+		$html .= '<label for="'.$params->name.'">'.$params->title.'</label>';
+		$html .= '<textarea id="'.$params->name.'" name="'.($params->prefix ?? '').str_replace('-','_',$params->name).'" class="'.$params->class.'" placeholder="'.$params->title.'" rows="'.$params->size.'"'.(isset($params->readonly) && $params->readonly ? ' readonly' : '').(isset($params->required) && $params->required ? ' required' : '').' tabindex="-1">'.($params->value ?? $params->default ?? '').'</textarea>';
+		$html .= '</div>';
+		return $html;
+	}
+	
+	// Filter for text search
+	public static function textFilter(&$params) {
+		!is_array($params) || $params = (object)$params;
+		return '<div class="form-group"><label for="'.$params->name.'">'.$params->label.'</label><input id="'.$params->prefix.str_replace('-','_',$params->name).'" name="'.$params->name.'" class="form-control" type="text" placeholder="'.$params->label.'" value="'.$params->value.'" /></div>';
 	}
 }
 ?>
