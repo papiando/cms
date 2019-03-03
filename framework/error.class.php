5ade7d11b10e42dc6d1b2ea7b520c83ac5308fb3
papiando (HOME) <info@papiando.com>
2019-03-03 14:27:09 -0400

    Update error.class.php

--- framework/error.class.php
+++ framework/error.class.php
@@ -3,38 +3,42 @@ namespace Cubo;
 
 defined('__CUBO__') || new \Exception("No use starting a class without an include");
 
+define('ERROR_FATAL',1);
+define('ERROR_CRITICAL',2);
+define('ERROR_SEVERE',3);
+define('ERROR_WARNING',4);
+
 class Error extends \Exception {
 	protected $_Error;
 	
 	// Constructor reads error and saves info as object
 	public function __construct($error) {
 		if(is_array($error)) {
-			$this->_error = (object)$error;
-			$this->_error->description = $this->_error->description ?? '<p>Please <a href="/contact/admin">contact</a> your site administrator.</p>';
+			$this->_Error = (object)$error;
+			$this->_Error->description = $this->_error->description ?? '<p>Please <a href="/contact/admin">contact</a> your site administrator.</p>';
 		} else {
-			$this->_error = (object)array('source'=>'Unknown','severity'=>3,'message'=>$error,'description'=>'<p>Please <a href="/contact/admin">contact</a> your site administrator.</p>');
+			$this->_Error = (object)['file'=>__FILE__,'severity'=>ERROR_SEVERE,'message'=>$error,'description'=>'<p>Please <a href="/contact/admin">contact</a> your site administrator.</p>'];
 		}
-		parent::__construct($this->_error->message ?? 'Unknown error',$this->_error->code ?? 0);
+		parent::__construct($this->_Error->message ?? 'Unknown error',$this->_Error->code ?? 0);
 	}
 	
 	// Custom string representation of error
 	public function __toString() {
-		return $this->_error->message;
+		return $this->_Error->message;
 	}
 	
 	// Display error template
 	public function showMessage() {
-		$_error = $this->_error;
 		if(defined('DEBUG')) {
-			$this->_error->description .= "<hr /><p>Error was thrown from:</p><table>";
-			if(isset($this->_error->class)) $this->_error->description .= "<tr><td>Class:</td><td>".($this->_error->class)."</td></tr>";
-			if(isset($this->_error->method)) $this->_error->description .= "<tr><td>Method:</td><td>".($this->_error->method)."</td></tr>";
-			if(isset($this->_error->function)) $this->_error->description .= "<tr><td>Function:</td><td>".($this->_error->function)."</td></tr>";
-			if(isset($this->_error->file)) $this->_error->description .= "<tr><td>Script:</td><td>".($this->_error->file)."</td></tr>";
-			if(isset($this->_error->line)) $this->_error->description .= "<tr><td>Line:</td><td>".($this->_error->line)."</td></tr>";
-			$this->_error->description .= "</table>";
+			$this->_Error->description .= "<hr /><p>Error was thrown from:</p><table>";
+			if(isset($this->_Error->class)) $this->_Error->description .= "<tr><td>Class:</td><td>".($this->_Error->class)."</td></tr>";
+			if(isset($this->_Error->method)) $this->_Error->description .= "<tr><td>Method:</td><td>".($this->_Error->method)."</td></tr>";
+			if(isset($this->_Error->function)) $this->_Error->description .= "<tr><td>Function:</td><td>".($this->_Error->function)."</td></tr>";
+			if(isset($this->_Error->file)) $this->_Error->description .= "<tr><td>Script:</td><td>".($this->_Error->file)."</td></tr>";
+			if(isset($this->_Error->line)) $this->_Error->description .= "<tr><td>Line:</td><td>".($this->_Error->line)."</td></tr>";
+			$this->_Error->description .= "</table>";
 		}
-		empty($this->_error->response) || http_response_code($this->_error->response);
+		empty($this->_Error->response) || http_response_code($this->_Error->response);
 		include(__ROOT__.DS.'error.php');
 		die();
 	}
