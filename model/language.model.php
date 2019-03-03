ed8fab30f60429dc4ff2c47618487f8ce6b28331
papiando (HOME) <info@papiando.com>
2019-03-03 16:06:20 -0400

    Update language.model.php

--- model/language.model.php
+++ model/language.model.php
@@ -1,8 +1,34 @@
 <?php
+/**
+ * @application    Cubo CMS
+ * @type           Model
+ * @class          Language
+ * @version        2.0.4
+ * @date           2019-03-03
+ * @author         Dan Barto
+ * @copyright      Copyright (c) 2019 Cubo CMS; see COPYRIGHT.md
+ * @license        MIT License; see LICENSE.md
+ */
 namespace Cubo;
 
-defined('__CUBO__') || new \Exception("No use starting a class without an include");
-
 class Language extends Model {
+	// Retrieve a single record from the model
+	public static function getLanguage($id) {
+		$columns = "`#`,`name`,`accesslevel`,`alpha2`,`alpha3`,`status`,`title`";
+		self::getDB()->select($columns)->from(strtolower(self::getClass()));
+		if(empty($id)) {
+			return null;								// Safety net if no valid $id is provided
+		} elseif(is_numeric($id)) {
+			self::getDB()->where("`#`=:id");			// A number was provided
+		} elseif(strlen($id) == 2) {
+			self::getDB()->where("`alpha2`=:id");		// An two letter alpha code was provided
+		} elseif(strlen($id) == 2) {
+			self::getDB()->where("`alpha3`=:id");		// An three letter alpha code was provided
+		} else {
+			self::getDB()->where("`name`=:id");
+		}
+		$result = self::getDB()->loadObject([':id'=>$id]);
+		return (is_object($result) ? $result : null);	// Only return the object, otherwise return nothing
+	}
 }
 ?>
