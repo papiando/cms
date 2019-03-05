7c461ed9baf29cf3a6f5643acf18f1a849b0a9b4
papiando (CBCS) <info@papiando.com>
2019-03-05 14:17:58 -0400

    Adding script headers

--- controller/category.controller.php
+++ controller/category.controller.php
@@ -1,7 +1,15 @@
 <?php
+/**
+ * @application    Cubo CMS
+ * @type           Controller
+ * @class          CategoryController
+ * @version        2.0.4
+ * @date           2019-03-05
+ * @author         Dan Barto
+ * @copyright      Copyright (c) 2019 Cubo CMS; see COPYRIGHT.md
+ * @license        MIT License; see LICENSE.md
+ */
 namespace Cubo;
-
-defined('__CUBO__') || new \Exception("No use starting a class without an include");
 
 class CategoryController extends Controller {
 	protected $columns = "*";
--- controller/template.controller.php
+++ controller/template.controller.php
@@ -1,7 +1,15 @@
 <?php
+/**
+ * @application    Cubo CMS
+ * @type           Controller
+ * @class          TemplateController
+ * @version        2.0.4
+ * @date           2019-03-05
+ * @author         Dan Barto
+ * @copyright      Copyright (c) 2019 Cubo CMS; see COPYRIGHT.md
+ * @license        MIT License; see LICENSE.md
+ */
 namespace Cubo;
-
-defined('__CUBO__') || new \Exception("No use starting a class without an include");
 
 class TemplateController extends Controller {
 	protected $columns = "name,accesslevel,script,status,style";
